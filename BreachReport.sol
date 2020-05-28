9c7e2002c21e2637eedaa5e8e5055ec334e589e5
marutiranjan <31069272+marutiranjan@users.noreply.github.com>
2020-05-28 12:17:34 +0530

    Update BreachReport.sol

--- BreachReport.sol
+++ BreachReport.sol
@@ -61,12 +61,6 @@ contract BreachReport {
       stopped = !stopped;
     }  
 
-    function breachInsert2(string memory _product,string memory _process,string memory _control,string memory _breachtimestamp,string memory _agentName,string  memory _conversationName,string memory _jurisdiction,string memory _breachName,string memory _regulator,string memory _clientDetails,string memory _breachDescription) public {
-        Breach memory breach = Breach(breachid,'INIT',_product,_process,_control,_breachtimestamp,_agentName,_conversationName,_jurisdiction,_breachName,_regulator,_clientDetails,_breachDescription,'SYSTEM');
-        breaches[breachid] = breach;
-        breachid +=1;
-    }     
-    
     function breachInsert(string[] memory breachFields,string[] memory breachValues) public {
         Breach memory breach = Breach(breachid,'INIT',breachValues[0],breachValues[1],breachValues[2],breachValues[3],breachValues[4],breachValues[5],breachValues[6],breachValues[7],breachValues[8],breachValues[9],breachValues[10],'SYSTEM');
         breaches[breachid] = breach;
@@ -81,29 +75,33 @@ contract BreachReport {
         }
     } 
 
-    function invalidateBreach(uint256 _id) public {
-        if(breaches[_id].id > 0) {
+    function inActivateBreach(uint256 _id) public {
+        if(breaches[_id].id == _id) {
             breaches[_id].state = 'INVALID';
         }
     }
     
+    function ActivateBreach(uint256 _id) public {
+        if(breaches[_id].id == _id) {
+            breaches[_id].state = 'INVALID';
+        }
+    }
+	
     function riskAndComplianceTeamSignOff(uint256 _id) public {
-    //    Breach memory breach = Breach(_id,_state,_product,_process,_control,_breachtimestamp,_agentName,_conversationName,_jurisdiction,_breachName,_regulator,_clientDetails,_breachDescription);
-        if(breaches[_id].id > 0) {
+
+        if(breaches[_id].id == _id) {
             breaches[_id].state = 'RCTSIGNOFF';
         }
     }  
 
     function auditorSignOff(uint256 _id) public {
-    //    Breach memory breach = Breach(_id,_state,_product,_process,_control,_breachtimestamp,_agentName,_conversationName,_jurisdiction,_breachName,_regulator,_clientDetails,_breachDescription);
-        if(breaches[_id].id > 0) {
+        if(breaches[_id].id == _id) {
             breaches[_id].state = 'AUDITSIGNOFF';
         }
     }  
 
     function regulatorAck(uint256 _id) public {
-    //    Breach memory breach = Breach(_id,_state,_product,_process,_control,_breachtimestamp,_agentName,_conversationName,_jurisdiction,_breachName,_regulator,_clientDetails,_breachDescription);
-        if(breaches[_id].id > 0) {
+        if(breaches[_id].id == _id) {
             breaches[_id].state = 'REGACK';
         }
     }  
@@ -144,25 +142,7 @@ contract BreachReport {
       }
      }     
 
-    function getBreach2(uint _id) public view returns (string memory breachDescription,string memory conversationName,string memory jurisdiction,string memory regulator,string memory agentName,string memory clientDetails,string memory updatedby){
-      if( breaches[0].id > 0) {
-
-            return (breaches[_id].breachDescription,breaches[_id].conversationName,breaches[_id].jurisdiction,breaches[_id].regulator,breaches[_id].agentName,breaches[_id].clientDetails,breaches[_id].updatedby);
-      }else{
-          return ("","","","","","","");
-      }
-     }     
-
-    function getBreach(uint _id) public view returns (string memory breachDescription,string memory conversationName,string memory jurisdiction,string memory regulator,string memory agentName,string memory clientDetails,string memory updatedby){
-      if( breaches[0].id > 0) {
-
-            return (breaches[_id].breachDescription,breaches[_id].conversationName,breaches[_id].jurisdiction,breaches[_id].regulator,breaches[_id].agentName,breaches[_id].clientDetails,breaches[_id].updatedby);
-      }else{
-          return ("","","","","","","");
-      }
-     }
-
-      function getBreaches() public view returns (uint[] memory breachId,string[] memory state,string[] memory control,string[] memory product,string[] memory process,string[] memory breachName){
+      function getAllBreaches() public view returns (uint[] memory breachId,string[] memory state,string[] memory control,string[] memory product,string[] memory process,string[] memory breachName){
           uint[] memory _id = new uint[](breachid-1);
           string[] memory _state = new string[](breachid-1);
           string[] memory _control = new string[](breachid-1);
@@ -186,5 +166,126 @@ contract BreachReport {
           }
           return (_id,_state,_control,_product,_process,_breachName);
       }	 
+	  
+      function getBreachesInRange(uint startId,uint range) public view returns (uint[] memory breachId,string[] memory state,string[] memory control,string[] memory product,string[] memory process,string[] memory breachName){
+          uint[] memory _id = new uint[](range);
+          string[] memory _state = new string[](range);
+          string[] memory _control = new string[](range);
+          string[] memory _product = new string[](range);
+          string[] memory _process = new string[](range);
+          string[] memory _breachName = new string[](range);
+		  
+		  uint bid = 0;
+          if (startId == 0){
+			bid = breachid-1;
+		  }else{
+		    bid = startId-1;
+		  }		  
+
+          uint j=0;
+          for (uint i = bid; j < range && i > 0 ; i--) {
+              if(breaches[i].id > 0){
+                _id[j] = breaches[i].id;  
+                _state[j] = breaches[i].state;
+                _control[j] = breaches[i].control;
+                _product[j] = breaches[i].product;
+                _process[j] = breaches[i].process;
+                _breachName[j] = breaches[i].breachName;
+                j +=1;
+              }
+          }
+          return (_id,_state,_control,_product,_process,_breachName);
+      }	
+	  
+    function getBreachDataForRegulator(uint _id) public view returns (uint breachid,string[] memory breachFields,string[] memory breachValues){
+      if( breaches[_id].id == _id && ( ((keccak256(bytes(breaches[_id].state)) == keccak256(bytes("AUDITSIGNOFF")))) ||  ((keccak256(bytes(breaches[_id].state)) == keccak256(bytes("REGACK")))) ) ) {
+          string [] memory _breachFields = new string[](13);
+          string [] memory _breachValues = new string[](13);
+          _breachFields[0] = 'state';
+		  _breachFields[1] = 'product';
+		  _breachFields[2] = 'process';
+		  _breachFields[3] = 'control';
+		  _breachFields[4] = 'breachtimestamp';
+		  _breachFields[5] = 'agentName';
+		  _breachFields[6] = 'conversationName';
+          _breachFields[7] = 'jurisdiction';
+		  _breachFields[8] = 'breachName';
+		  _breachFields[9] = 'regulator';
+		  _breachFields[10] = 'clientDetails';
+		  _breachFields[11] = 'breachDescription';
+		  _breachFields[12] = 'updatedby';
+		  
+		  _breachValues[0] = breaches[_id].state;
+		  _breachValues[1] = breaches[_id].product;
+		  _breachValues[2] = breaches[_id].process;
+		  _breachValues[3] = breaches[_id].control;
+		  _breachValues[4] = breaches[_id].breachtimestamp;
+		  _breachValues[5] = breaches[_id].agentName;
+		  _breachValues[6] = breaches[_id].conversationName;
+          _breachValues[7] = breaches[_id].jurisdiction;
+		  _breachValues[8] = breaches[_id].breachName;
+		  _breachValues[9] = breaches[_id].regulator;
+		  _breachValues[10] = breaches[_id].clientDetails;
+		  _breachValues[11] = breaches[_id].breachDescription;
+		  _breachValues[12] = breaches[_id].updatedby;
+		  
+            return (_id,_breachFields,_breachValues);
+      }
+     }     
+
+      function getAllBreachesForRegulator() public view returns (uint[] memory breachId,string[] memory state,string[] memory control,string[] memory product,string[] memory process,string[] memory breachName){
+          uint[] memory _id = new uint[](breachid-1);
+          string[] memory _state = new string[](breachid-1);
+          string[] memory _control = new string[](breachid-1);
+          string[] memory _product = new string[](breachid-1);
+          string[] memory _process = new string[](breachid-1);
+          string[] memory _breachName = new string[](breachid-1);
+
+
+
+          uint j=0;
+          for (uint i = breachid-1; i > 0; i--) {
+              if(breaches[i].id == i && ( ((keccak256(bytes(breaches[i].state)) == keccak256(bytes("AUDITSIGNOFF")))) ||  ((keccak256(bytes(breaches[i].state)) == keccak256(bytes("REGACK")))) )){
+                _id[j] = breaches[i].id;  
+                _state[j] = breaches[i].state;
+                _control[j] = breaches[i].control;
+                _product[j] = breaches[i].product;
+                _process[j] = breaches[i].process;
+                _breachName[j] = breaches[i].breachName;
+                j +=1;
+              }
+          }
+          return (_id,_state,_control,_product,_process,_breachName);
+      }	 
+	  
+      function getBreachesInRangeForRegulator(uint startId,uint range) public view returns (uint[] memory breachId,string[] memory state,string[] memory control,string[] memory product,string[] memory process,string[] memory breachName){
+          uint[] memory _id = new uint[](range);
+          string[] memory _state = new string[](range);
+          string[] memory _control = new string[](range);
+          string[] memory _product = new string[](range);
+          string[] memory _process = new string[](range);
+          string[] memory _breachName = new string[](range);
+		  
+		  uint bid = 0;
+          if (startId == 0){
+			bid = breachid-1;
+		  }else{
+		    bid = startId-1;
+		  }		  
+
+          uint j=0;
+          for (uint i = bid; j < range && i > 0 ; i--) {
+              if(breaches[i].id == i && ( ((keccak256(bytes(breaches[i].state)) == keccak256(bytes("AUDITSIGNOFF")))) ||  ((keccak256(bytes(breaches[i].state)) == keccak256(bytes("REGACK")))) )){
+                _id[j] = breaches[i].id;  
+                _state[j] = breaches[i].state;
+                _control[j] = breaches[i].control;
+                _product[j] = breaches[i].product;
+                _process[j] = breaches[i].process;
+                _breachName[j] = breaches[i].breachName;
+                j +=1;
+              }
+          }
+          return (_id,_state,_control,_product,_process,_breachName);
+      }	
 
 }
